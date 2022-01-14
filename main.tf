9ece45b69736abe7a4be693d52f10f82647bed4b
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2022-01-14 17:06:03 +0000

    adding output descriptions and license file

--- main.tf
+++ main.tf
@@ -1,21 +1,27 @@
-terraform {
-  required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.10.0"
-    }
+locals {
+  user_names = tolist(data.azuread_users.users.user_principal_names)
+  object_ids = tolist(data.azuread_users.users.object_ids)
+
+  azure_users = zipmap(local.user_names, local.object_ids)
+
+}
+
+resource "vault_identity_entity" "azure_users" {
+  for_each = local.azure_users
+
+
+  name     = each.key
+  policies = var.policies
+
+  metadata = {
+    version = "1"
   }
 }
 
-provider "vault" {
-  address = "http://localhost:8200"
-  token   = var.vault_token
-}
+resource "vault_identity_entity_alias" "user_alias" {
+  for_each = local.azure_users
 
-provider "azuread" {}
-
-variable "vault_token" {}
-
-module "users" {
-  source = "../"
+  name           = each.value
+  mount_accessor = data.vault_auth_backend.auth_method.accessor
+  canonical_id   = vault_identity_entity.azure_users[each.key].id
 }
